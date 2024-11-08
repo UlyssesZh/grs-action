4fa4b65c8b7db4d227f42d59ff39b368f1845a71
Ulysses Zhan <UlyssesZhan@gmail.com>
2024-11-08 00:01:21 -0800

    use composite action

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 #!/usr/bin/env node
 
-import { getInput, setFailed } from '@actions/core'
 import githubReadmeStats from './api.js'
 import { mkdirSync, writeFileSync } from 'fs'
 import { dirname } from 'path'
 
-const DEFAULT_DIR = 'grs';
 const CARDS = ['stats', 'repo', 'langs', 'wakatime', 'gist'];
 
-try {
-	const grsOptions = getInput('options', { required: true });
-	const card = getInput('card', { required: false }) ?? 'stats';
-	const targetPath = getInput('path');
+const grsOptions = process.env.GRS_OPTIONS;
+const card = process.env.GRS_CARD;
+const targetPath = process.env.GRS_PATH;
 
-	mkdirSync(targetPath ? dirname(targetPath) : DEFAULT_DIR, { recursive: true });
-	for (const cardType of CARDS) {
-		if (card !== cardType) {
-			continue;
-		}
-		console.log(process.env.PAT_1)
-		githubReadmeStats[cardType](grsOptions).then(svg => {
-			writeFileSync(targetPath ?? `${DEFAULT_DIR}/${cardType}.svg`, svg)
-		});
-		break;
+mkdirSync(targetPath ? dirname(targetPath) : DEFAULT_DIR, { recursive: true });
+for (const cardType of CARDS) {
+	if (card !== cardType) {
+		continue;
 	}
-} catch (error) {
-	setFailed(error.message);
+	console.log(process.env.PAT_1)
+	githubReadmeStats[cardType](grsOptions).then(svg => {
+		writeFileSync(targetPath, svg)
+	});
+	break;
 }
